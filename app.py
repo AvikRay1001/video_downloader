1653031ffee4d5f3be6753ebb3c28643f041ac67
AvikRay1001 <avikray1001@gmail.com>
2026-03-06 23:50:56 +0530

    first commit

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
 from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
 import instaloader
 import os
 import glob
-import uuid
 
 app = Flask(__name__)
+CORS(app)
 
 DOWNLOAD_FOLDER = "temp"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
-def download_instagram_video(post_url):
-
-    loader = instaloader.Instaloader(
-        dirname_pattern=DOWNLOAD_FOLDER,
-        download_pictures=False,
-        download_video_thumbnails=False,
-        save_metadata=False
-    )
-
-    shortcode = post_url.split("/")[-2]
-
-    post = instaloader.Post.from_shortcode(loader.context, shortcode)
-
-    loader.download_post(post, target=DOWNLOAD_FOLDER)
-
-    files = glob.glob(f"{DOWNLOAD_FOLDER}/*.mp4")
-
-    if files:
-        return files[-1]
-
-    return None
-
+@app.route("/")
+def home():
+    return {"status": "API running"}
 
 @app.route("/download", methods=["POST"])
 def download():
@@ -38,16 +20,19 @@ def download():
     data = request.json
     url = data.get("url")
 
-    if not url:
-        return jsonify({"error": "No URL provided"}), 400
+    loader = instaloader.Instaloader(
+        dirname_pattern=DOWNLOAD_FOLDER,
+        download_pictures=False
+    )
 
-    video_path = download_instagram_video(url)
+    shortcode = url.split("/")[-2]
+    post = instaloader.Post.from_shortcode(loader.context, shortcode)
 
-    if not video_path:
-        return jsonify({"error": "Download failed"}), 500
+    loader.download_post(post, target=DOWNLOAD_FOLDER)
 
-    return send_file(video_path, as_attachment=True)
+    files = glob.glob(f"{DOWNLOAD_FOLDER}/*.mp4")
 
+    if not files:
+        return jsonify({"error": "Download failed"})
 
-if __name__ == "__main__":
-    app.run()
+    return send_file(files[-1], as_attachment=True)
